359374eed2ab2e3645c50b7b1f81028874e42189
Dmitry Dyachkov <git@e965.ru>
2023-01-06 12:41:02 +0500

    scheme basic math example

---
+++ scripts/scheme/basic-math.scm
@@ -0,0 +1,12 @@
+(display "sum: ")
+(write (+ 100 200 300)) ; 100 + 200 + 300 = 600
+
+(display "\n") ; перенос строки
+
+(display "multiplication: ")
+(write (* 100 3 4 5)) ; 100 * 3 * 4 * 5 = 6000
+
+(display "\n") ; перенос строки
+
+(display "compound expression: ")
+(write (* (/ (+ 1 2) (* 3 4)) 4)) ; (1 + 2) / (3 * 4) * 4 = 3 / 12 * 4 = 1/4 * 4 = 1
